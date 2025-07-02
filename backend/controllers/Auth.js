f453a33995bd92c14b4ce76e47b9a5f5a7a20110
Nithin0620 <nithin30302@gmail.com>
2025-07-02 23:27:24 +0530

    Auth Backend

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,10 +1,14 @@
 const User = require("../modals/user")
 const OTP = require("../modals/otp")
+const bcrypt = require("bcrypt")
+const gravatar = require('gravatar');
 
-export const signup = ({firstName , lastName , email , password , confirmPassword , agreetermsofservice})=>{
+export const signup = async(req,res)=>{
    try{
-      if(!firstName , !lastName , !email , !password , !confirmPassword , !agreetermsofservice){
-         return res.status(400).json({
+      const {firstName , lastName , email , password , confirmPassword , agreetermsofservice , otp} = req.body;
+
+      if(!firstName || !lastName || !email || !password || !confirmPassword || !agreetermsofservice ,!otp){
+         return res.status(403).json({
             success:false,
             message:"All fields are required",
          })
@@ -16,8 +20,47 @@ export const signup = ({firstName , lastName , email , password , confirmPasswor
             message:"password and Confirm Password does not match",
          })
       }
+
+      const existingUser = await User.findOne({email:email});
+      if (existingUser) {
+         return res.status(400).json({
+            success: false,
+            message: "User already exist's",
+         });
+      }
+      const recentOtp = await OTP.findOne({emai:email}).sort({createdAt:-1}).limit(1);
+      if(recentOtp.length === 0){
+         return res.status(400).json({
+            success: false,
+            message: "The OTP is not valid",
+         })
+      }
+      else if(recentOtp.otp !== otp){
+         return res.status(400).json({
+            success:false,
+            message:"Invalid OTP",
+         })
+      }
+      const hashedPassword = await bcrypt.hash(password , 10);
+      const profilePic = `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
+
+      const payload = {firstName , lastName , email , hashedPassword , profilePic}
+      const response = await User.create(payload);
+
+      if(!response) {
+         throw new Error("Error occured in creating new User")
+      }
+      
+      return res.status(200).json({
+         success:true,
+         message:"User created Successfully",
+         data:response,
+      })      
    }
    catch(e){
-
+      return res.status(500).json({
+         success:false,
+         message:"Error occured in the backend in the Signup controller", 
+      })
    }
 }
