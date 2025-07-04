e6f332ccf2a304d20bd6e1c40a71e37d1cc82946
Nithin0620 <nithin30302@gmail.com>
2025-07-04 22:53:24 +0530

    Post controllers done

--- backend/controllers/Post.js
+++ backend/controllers/Post.js
@@ -172,29 +172,140 @@ export const deletePost = async (req, res) => {
 
 export const getAllPosts = async (req, res) => {
   try {
-      const response = await Post.find()
+    const response = await Post.find()
       .populate("author", "firstName lastName image")
-      .populate("categories" , "name")
-      .populate("likes" , "firstName lastName")
+      .populate("categories", "name")
+      .populate("likes", "firstName lastName")
       .populate({
-         path:"comments",
-         populate:{path:"user" , select: "firstName lastName image"}
+        path: "comments",
+        populate: { path: "user", select: "firstName lastName image" },
       })
       .exec();
 
-      const finalResponse = response.sort({createdAt:-1});  //jo naya, vo  pehle ayega 
+    const finalResponse = response.sort({ createdAt: -1 }); //jo naya, vo  pehle ayega
+
+    return res.status(200).json({
+      success: true,
+      message: "All posts fetched successfully",
+      data: finalResponse,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: "error in fetching all the posts",
+    });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const response = await Post.findById(postId)
+      .populate("author", "firstName lastName image")
+      .populate("categories", "name")
+      .populate("likes", "firstName lastName")
+      .populate({
+        path: "comments",
+        populate: { path: "user", select: "firstName lastName image" },
+      })
+      .sort({createdAt:-1})
+      .exec();
+
+    return res.status(200).json({
+      success: true,
+      message: " posts fetched by Id successfully",
+      data: response,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: "error in fetching all the posts",
+    });
+  }
+};
+
+export const getPostByCategory = async (req, res) => {
+  try {
+      const categoryName = req.params.category;
+
+      const category = await Category.findOne({ name: categoryName }).populate({
+         path: "posts",
+         options: { sort: { createdAt: -1 } },
+         populate: [
+            { path: "author", select: "firstName lastName image" },
+            { path: "categories", select: "name" },
+            { path: "likes", select: "firstName lastName" },
+            {
+               path: "comments",
+               populate: { path: "user", select: "firstName lastName image" },
+            },
+         ],
+      });
+
+      if (!category) {
+         return res.status(404).json({
+         success: false,
+         message: "Category not found",
+         });
+      }
 
       return res.status(200).json({
          success: true,
-         message: "All posts fetched successfully",
-         data: finalResponse,
+         message: "Posts fetched by category successfully",
+         data: category,
       });
-   } 
+   }
    catch (e) {
       console.log(e);
       return res.status(500).json({
          success: false,
-         message: "error in fetching all the posts",
+         message: "Error in fetching the posts by category",
+      });
+   }
+};
+
+export const getPostByUser = async(req,res)=>{
+   try{
+      const userId = req.params.id;
+
+      const user = await User.findById(userId);
+
+      if(!user){
+         return res.status(404).json({
+            success:false,
+            message:"User not found",
+         })
+      }
+
+      const profileId = user.profile;
+
+      const response = await Profile.findById(profileId).populate({
+         path: "posts",
+         options: { sort: { createdAt: -1 } },
+         populate: [
+            { path: "author", select: "firstName lastName image" },
+            { path: "categories", select: "name" },
+            { path: "likes", select: "firstName lastName" },
+            {
+               path: "comments",
+               populate: { path: "user", select: "firstName lastName image" },
+            },
+         ],
+      })
+      return res.status(200).json({
+         success: true,
+         message: "Posts fetched by User Id successfully",
+         data: response.posts,
+      });
+   }
+   catch (e) {
+      console.log(e);
+      return res.status(500).json({
+         success: false,
+         message: "Error in fetching the posts by User Id",
       });
    }
 };
